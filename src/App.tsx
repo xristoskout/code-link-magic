4b7b7591aaad2ac3cf63d7623be82cc8852a732d
xristoskout <31740784+xristoskout@users.noreply.github.com>
2025-07-17 10:53:19 +0300

    Remove Nissan Fotiadis pages and keep Mr Booking

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Cars from "./pages/Cars";
-import CarDetails from "./pages/CarDetails";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 import MrBooking from "./pages/MrBooking";
@@ -22,12 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="/cars" element={<Cars />} />
-            <Route path="/cars/:id" element={<CarDetails />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/mr-booking" element={<MrBooking />} />
+            <Route path="/" element={<MrBooking />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
